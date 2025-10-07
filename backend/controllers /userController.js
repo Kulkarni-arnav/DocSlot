30016376a1071664527bf89a902789487d7302e7
Kulkarni-arnav <kulkarniarnav1010@gmail.com>
2025-10-07 23:05:50 +0530

    added payment logic using razorpay and updated the payment status and cancel button

--- backend/controllers /userController.js
+++ backend/controllers /userController.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import {v2 as cloudinary} from 'cloudinary';
 import doctorModel from '../models/doctorModel.js';
 import appointmentModel from '../models/appointmentModel.js';
+import razorpay from 'razorpay';
 
 //Api to register a user
 const registerUser = async (req ,res)=>{
@@ -222,5 +223,57 @@ const cancelAppointment = async (req ,res)=>{
         res.json({ success:false, message: error.message });
     }
 }
+
+const razorpayInstance = new razorpay({
+    key_id:process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_KEY_SECRET,
+});
+
+// Api to make payment using razorpay
+
+const paymentRazorpay = async (req ,res)=>{
+
+    try {
+        const {appointmentId} = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+        if(!appointmentData||appointmentData.cancelled){
+            return res.json({success:false ,message : "No appointment found"});
+        }
+
+        //creating options for razorpay payments
+        const options = {
+            amount: appointmentData.amount * 100, //amount in paise
+            currency: process.env.CURRENCY,
+            receipt: appointmentId,
+        }
+
+        //creating order
+        const order = await razorpayInstance.orders.create(options);
+        res.json({success:true ,order})
+    } catch (error) {
+        console.log(error);
+        res.json({ success:false, message: error.message });
+    }
+}
+
+//Api to varify payment of razorpay
+
+const verifyRazorpay = async (req ,res)=>{
+    try {
+        const {razorpay_order_id} = req.body;
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+        if(orderInfo.status==='paid'){
+            await appointmentModel.findByIdAndUpdate(orderInfo.receipt , {payment : true});
+            res.json({success:true ,message : "Payment successful"})
+        }else{
+            res.json({success:false ,message : "Payment not successful"})
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+        res.json({ success:false, message: error.message });
+    }
+}
  
-export {registerUser,loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment};
+export {registerUser,loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay};
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -3,13 +3,14 @@ import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MyAppointments = () => {
 
   const {backendUrl,token,getDoctorsData} = useContext(AppContext)
   const [appointments,setAppointments] = useState([])
 
-  
+  const navigate = useNavigate()
 
   const getUserAppointments = async()=>{
     try {
@@ -44,6 +45,50 @@ const MyAppointments = () => {
     }
   }
 
+  const initPay = (order) =>{
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency ,
+      name: 'Appointment Payment',
+      description: 'Appointment Payment',
+      order_id: order.id,
+      reciept: order.receipt,
+      handler: async(response)=>{
+        console.log(response);
+        
+        try {
+          const {data} = await axios.post(backendUrl+'/api/user/verifyRazorpay',response,{headers:{token}})
+          if(data.success){
+            getUserAppointments()
+            navigate('/my-appointments')
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error(error.message)
+        }
+
+      }
+    }
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+
+  }
+
+  const appointmentRazorpay = async(appointmentId)=>{
+    try {
+      const {data} = await axios.post(backendUrl+'/api/user/payment-razorpay',{appointmentId},{headers:{token}})
+      if(data.success){
+        initPay(data.order)
+      }
+        
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
   useEffect(()=>{
 if (token) {
   getUserAppointments()
@@ -71,7 +116,8 @@ if (token) {
               </div>
               <div></div>
               <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+                {!item.cancelled && item.payment && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
+                {!item.cancelled && !item.payment && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
                 {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
                 {item.cancelled && <p className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</p>}
               </div>
