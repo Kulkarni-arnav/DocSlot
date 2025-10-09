7b8135ea6e61de29ccb6e67331816e7dc2e816c0
Kulkarni-arnav <kulkarniarnav1010@gmail.com>
2025-10-09 16:35:03 +0530

    Created admin Dashboard with all required info

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,6 +11,7 @@ const AdminContextProvider =(props)=>{
     const [aToken , setAToken] =useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] =useState([])
     const [appointments, setAppointments] =useState([])
+    const [dashData, setDashData] =useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -74,6 +75,20 @@ const AdminContextProvider =(props)=>{
         }
     }
 
+    const getDashData = async()=>{
+        try {
+            const {data} = await axios.get(backendUrl+'/api/admin/dashboard',{headers:{aToken}})
+            if(data?.success){
+                setDashData(data.dashData)
+                console.log(data.dashData);
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value ={
         aToken,
         setAToken,
@@ -84,7 +99,9 @@ const AdminContextProvider =(props)=>{
         appointments,
         setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,
+        getDashData
     }
     return <AdminContext.Provider value={value}>
         {props.children}
