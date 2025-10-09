2ffc3c7c30f71886dcd5c870db07cf22391856cf
Kulkarni-arnav <kulkarniarnav1010@gmail.com>
2025-10-09 15:41:42 +0530

    Added appointments page in admin panel showing all appointments and enabled cancel appointment function to the admins

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { set } from "mongoose";
 
 export const AdminContext =createContext()
 
@@ -9,6 +10,7 @@ const AdminContextProvider =(props)=>{
 
     const [aToken , setAToken] =useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] =useState([])
+    const [appointments, setAppointments] =useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -42,13 +44,47 @@ const AdminContextProvider =(props)=>{
         }
     }
 
+    const getAllAppointments = async()=>{
+        try {
+            const {data} =await axios.get(backendUrl+'/api/admin/appointments',{headers:{aToken}})
+            if(data?.success){
+                setAppointments(data.appointments)
+                console.log(data.appointments);
+                
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async(appointmentId)=>{
+        try {
+            const {data} = await axios.post(backendUrl+'/api/admin/cancel-appointment',{appointmentId},{headers:{aToken}})
+            if(data?.success){
+                toast.success(data.message)
+                getAllAppointments()
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value ={
         aToken,
         setAToken,
         backendUrl,
         doctors,
         getAllDoctors,
-        changeAvailability
+        changeAvailability,
+        appointments,
+        setAppointments,
+        getAllAppointments,
+        cancelAppointment
     }
     return <AdminContext.Provider value={value}>
         {props.children}
